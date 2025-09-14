7f58143fc2d9992a892120b0b080205422675454
mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2025-09-14 17:31:22 +0900

    Add narrator

--- models/sentence.py
+++ models/sentence.py
@@ -1,3 +1,8 @@
+from typing import Optional
+from models.narrator import Narrator
+
 class Sentence:
+    narrator: Optional[Narrator] = None
+    
     def __init__(self, sentence: str):
         self.sentence = sentence
