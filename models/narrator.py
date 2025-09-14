bb10a331da0dd90e0d400b2c6649387efc1d075f
mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2025-09-15 00:32:50 +0900

    Add Gender

--- models/narrator.py
+++ models/narrator.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from pydantic import BaseModel
 
+from models.gender import Gender
+
 class Narrator(BaseModel):
     name: str
-    gender: Optional[str] = None
+    gender: Optional[Gender] = None
