cc6a26c7f1600e8e9879358b452c9a6b85b35454
mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2025-09-14 17:47:38 +0900

    余計な文字の除去

--- models/novel.py
+++ models/novel.py
@@ -11,4 +11,4 @@ class Novel:
             lines = f.readlines()
         
         for line in lines:
-            self.sentences.append(Sentence(line))
+            self.sentences.append(Sentence(line.strip()))
