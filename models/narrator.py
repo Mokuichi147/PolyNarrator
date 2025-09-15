e8a0218e01afc321c186fc42b343277870eb7028
mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2025-09-15 15:37:50 +0900

    Add aliases

--- models/narrator.py
+++ models/narrator.py
@@ -1,8 +1,9 @@
-from typing import Optional
+from typing import List, Optional
 from pydantic import BaseModel
 
 from models.gender import Gender
 
 class Narrator(BaseModel):
     name: str
+    aliases: List[str] = []
     gender: Optional[Gender] = None
