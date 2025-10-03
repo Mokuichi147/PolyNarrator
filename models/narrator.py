9eef711c6c825818827bd031f6e7e4372b56ca5e
mokuichi147 <38586357+Mokuichi147@users.noreply.github.com>
2025-10-04 00:54:37 +0900

    Add Jsonスキーマでの説明文

--- models/narrator.py
+++ models/narrator.py
@@ -1,10 +1,12 @@
-from typing import List, Optional
-from pydantic import BaseModel
+from typing import Annotated, List, Optional
+from pydantic import BaseModel, Field
 
 from models.gender import Gender
 
 class Narrator(BaseModel):
-    name: str
-    portrait: str
-    aliases: List[str] = []
-    gender: Optional[Gender] = None
+    """登場人物の情報。これらの情報を元に会話内容の発言者を推測する"""
+    
+    name: Annotated[str, Field(description="キャラクター名。正式名称であることが好ましい")]
+    portrait: Annotated[str, Field(description="性格や来歴、外見といったキャラクターの特徴")]
+    aliases: Annotated[List[str], Field(description="ニックネームや別称など")] = []
+    gender: Annotated[Optional[Gender], Field(description="性別")] = None
